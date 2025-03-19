163959dd73ee843046f221b0e075bd785f76d760
techmefr <gaetan-compigni@wilder.school>
2025-03-19 11:52:38 +0100

    learn watch

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,25 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, watch } from 'vue';
 
-//Var
-const msg = ref('tuto vue.js');
-const fName = ref('Anthony');
-const lName = ref('Cardinale');
-const price = ref(100);
-const discount = ref(30);
+const msg = ref('Tuto Vue.js');
+const x = ref(0);
+const dbg = ref('');
+const locked= ref(false)
 
-//Func
-// const fullName = computed(() => fName.value + " " + lName.value.toUpperCase());
-const finalPrice = computed(() => price.value - (price.value * discount.value / 100));
-
-const fullName = computed({
-  // getter
-  get(previous) {
-    if (1==1) {
-      return fName.value + ' ' + lName.value.toUpperCase();
-    }
-    return previous
-  },
-  // setter
-  set(newValue) {
-    [fName.value, lName.value] = newValue.split(' ')
+watch(x, (newX, oldX) => {
+  dbg.value = 'x = ' + newX+ " | old = "+oldX;
+  if (newX>5){
+    locked.value = true
+    // on ne peut plus modifier la valeur si celle-ci dépasse 5
   }
 });
 </script>
 
 <template>
   <main id="main">
-   <h1>{{ msg }}</h1>
-   <h2>Bonjour {{ fullName }}</h2>
-   <h3>Prix final : {{ finalPrice }}€</h3>
+    <h1>{{ msg }}</h1>
+    <p>{{ dbg }}</p>
+    <input :disabled="locked" type="number" v-model="x">
   </main>
 </template>
 
