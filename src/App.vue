174776e361f708e312b2a3a91afb30d65d23e74d
techmefr <gaetan-compigni@wilder.school>
2025-03-19 11:21:01 +0100

    test computed to calculate final price and firstname + lastanme

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,23 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 //Var
-const msg = ref('Tuto Vue.Js')
-const items = ref(['Item 1', 'Item 2', 'Item 3'])
-let id = 0
-
-const goodItems = ref([
-  { id: id++, text: 'Olivier' },
-  { id: id++, text: 'Tom' },
-  { id: id++, text: 'Marc' }
-])
-
-const newCharacter = ref('');
-const characters = ref([
-  { id: id++, text: 'Dark' },
-  { id: id++, text: 'Luke' }
-])
+const msg = ref('tuto vue.js');
+const fName = ref('Anthony');
+const lName = ref('Cardinale');
+const price = ref(100);
+const discount = ref(30);
 
 //Func
-const addCharacter = () => {
-  if (newCharacter.value.trim()) {
-    characters.value.push({ id: id++, text: newCharacter.value })
-    newCharacter.value = ''
-  }
-}
-
-const removeCharacter = (character) => {
-  characters.value = characters.value.filter((item) => item.id !== character.id)
-}
+const fullName = computed(() => fName.value + " " + lName.value.toUpperCase());
+const finalPrice = computed(() => price.value - (price.value * discount.value / 100));
 </script>
 
 <template>
   <main id="main">
-    <h1>{{ msg }}</h1>
-    <ul>
-      <li>Toto</li>
-      <li>Titi</li>
-    </ul>
-
-    <ul>
-      <li v-for="item in items" :key="item">{{ item }}</li>
-    </ul>
-
-    <ul>
-      <li v-for="goodItem in goodItems" :key="goodItem.id">{{ goodItem.text }}</li>
-    </ul>
-
-    <div>
-      <h2>Personnages</h2>
-      <ul>
-        <li v-for="character in characters" :key="character.id">
-          {{ character.text }} 
-          <button @click="removeCharacter(character)">X</button>
-        </li>
-      </ul>
-      <input type="text" v-model="newCharacter">
-      <button @click="addCharacter">Add character</button>
-    </div>
+   <h1>{{ msg }}</h1>
+   <h2>Bonjour {{ fullName }}</h2>
+   <h3>Prix final : {{ finalPrice }}€</h3>
   </main>
 </template>
 
