a683d6d6a96c64697b07fefe813a2ee036672aec
techmefr <gaetan-compigni@wilder.school>
2025-03-20 12:57:35 +0100

    add example to use slot

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,19 +3,22 @@ import { ref } from 'vue';
 import SubComp from './SubComp.vue';
 
 const msg = ref('Hello from component')
-const childMsg = ref('No msg')
 </script>
 
 <template>
     <h1>{{ msg }}</h1>
-    <h3>{{ childMsg }}</h3>
-<SubComp @compMsg="(msg)=> childMsg=msg" msg="Test msg"/>
-<SubComp msg="Toto"/>
-<SubComp :msg="msg" color="red"/>
+    <!-- Utilisation de la variable msg -->
+    <SubComp> Example : {{ msg.toUpperCase() }}</SubComp>
+
+    <!-- Utilisation du slot de l'enfant -->
+    <SubComp> </SubComp>
+    <SubComp/>
+
 
 </template>
 
 <style scoped>
 h1 {
     color: red;
-}</style>
+}
+</style>
--- src/components/SubComp.vue
+++ src/components/SubComp.vue
@@ -1,40 +1,9 @@
-<script setup>
-const props = defineProps({
-    msg: {
-        type: String,
-        required: false,
-        default: "Titi"
-    },
-    color: {
-        type: String,
-        required: false
-    }
-});
-
-// Définit ce qu'on peut émettre au parent
-const emit = defineEmits(['compMsg'])
-
-const clickHandler = () => {
-    console.log("Click" + " " + props.msg)
-    emit('compMsg', props.msg)
-}
-</script>
 
 <template>
-    <h2 :class="color">{{ msg || "Default" }}</h2>
-    <button @click="clickHandler">Click</button>
+    <h2>
+        <slot>
+            Default txt
+        </slot>
+    </h2>
 </template>
 
-<style scoped>
-h1 {
-    color: rebeccapurple;
-}
-
-.red {
-    color: red !important;
-}
-
-.blue {
-    color: blue !important;
-}
-</style>
